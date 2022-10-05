d3682381b4585e3e738c4c5ad51b897eac3d11b7
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-10-05 12:52:27 +0000

    更新

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -33,15 +33,15 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    if @item.update (item_params)
-    redirect_to item_path, notice: 'Item information updated successfully'
-    else
-    if @item.update(item_params)
-        redirect_to item_path(@item.id)
-    else
-    render :edit , status: :unprocessable_entity
-    end
-    end
+    @item.update (item_params)
+    redirect_to admin_item_path(@item.id), notice: 'Item information updated successfully'
+    #else
+    #if @item.update(item_params)
+     #   redirect_to admin_item_path(@item.id)
+    #else
+    #render :edit , status: :unprocessable_entity
+
+
   end
 
   private
