60457786dc91c8edcd32230b0733720520951158
EC2 Default User <ec2-user@ip-172-31-25-60.ap-northeast-1.compute.internal>
2022-12-10 13:04:09 +0000

    編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,19 +7,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-     @cart_item = CartItem.new(params_cart_item)
-      @cart_item.customer_id=current_customer.id
-      @cart_items=current_customer.cart_items.all
+     @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_items = current_customer.cart_items.all
       @cart_items.each do |cart_item|
-     if cart_item.item_id==@cart_item.item_id
+     if cart_item.item_id == @cart_item.item_id
        new_quantity = cart_item.quantity + @cart_item.quantity
        cart_item.update_attribute(:quantity, new_quantity)
-       @cart_item.delete
      end
-
-   end
        @cart_item.save
        redirect_to public_cart_items_path,notice:"カートに商品が入りました"
+     end
   end
 
   def update
@@ -48,8 +46,8 @@ class Public::CartItemsController < ApplicationController
 
    private
 
-  def params_cart_item
-     params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+  def cart_item_params
+     params.require(:cart_item).permit(:item_id, :amount)
   end
 end
 
